468bfea04e59e5938b58c28ba9074234f332e8a7
devid-corsard <truegrinde@gmail.com>
2023-07-08 16:11:34 +0300

    fix: luasnip deleted cmp configuration as unneccesary

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,23 +1,5 @@
-require 'cmp'.setup {
-    snippet = {
-        expand = function(args)
-            require 'luasnip'.lsp_expand(args.body)
-        end
-    },
-
-    sources = {
-        { name = 'luasnip' },
-        -- more sources
-    },
-}
--- require("luasnip.loaders.from_vscode").load({
---     paths = { "~/.local/share/mynvim/site/pack/packer/start/friendly-snippets" } })
---
--- require("luasnip.loaders.from_vscode").load({
---     paths = { "~/.local/share/mynvim/site/pack/packer/start/vim-react-snippets" } })
 require('luasnip').filetype_extend("javascript", { "javascriptreact", "typescriptreact" })
 require('luasnip').filetype_extend("javascript", { "html" })
 require('luasnip').filetype_extend("javascriptreact", { "html" })
 require('luasnip').filetype_extend("typescriptreact", { "html" })
-
 require("luasnip.loaders.from_vscode").lazy_load()
