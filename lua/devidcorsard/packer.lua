4c534830268dfdfee78dc0e2d67a95ee86f5a59b
devid-corsard <truegrinde@gmail.com>
2023-07-21 16:28:42 +0300

    fix: fixed order of installation

--- lua/devidcorsard/packer.lua
+++ lua/devidcorsard/packer.lua
@@ -22,6 +22,17 @@ return require('packer').startup(function(use)
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
+    use("rafamadriz/friendly-snippets")
+    use({
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!:).
+        run = "make install_jsregexp",
+        dependencies = "rafamadriz/friendly-snippets"
+    })
+    use { 'saadparwaiz1/cmp_luasnip' }
+    use('mlaursen/vim-react-snippets')
 
     use {
         'VonHeikemen/lsp-zero.nvim',
@@ -50,18 +61,6 @@ return require('packer').startup(function(use)
             require('Comment').setup()
         end
     }
-    use({
-        "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        tag = "v1.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!:).
-        run = "make install_jsregexp",
-        dependencies = "rafamadriz/friendly-snippets"
-    })
-    use { 'saadparwaiz1/cmp_luasnip' }
-
-    use("rafamadriz/friendly-snippets")
-    use('mlaursen/vim-react-snippets')
 
 
     use({
