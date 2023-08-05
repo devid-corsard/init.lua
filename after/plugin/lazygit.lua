1f91eb42f2f4483606abc0be4492451d4fb549fe
devid-corsard <truegrinde@gmail.com>
2023-08-05 16:39:15 +0300

    make fullscreen telescope and lazygit

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,2 +1,3 @@
-
 vim.keymap.set("n", "<leader>gg", vim.cmd.LazyGit)
+vim.g.lazygit_floating_window_scaling_factor = 1.0 -- scaling factor for floating window
+
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,12 @@
+require('telescope').setup{
+    defaults = {
+        layout_config = {
+            height = 0.99,
+            width = 0.99,
+        }
+    },
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
