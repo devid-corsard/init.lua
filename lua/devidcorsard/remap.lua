c1d059a116131dec0263a2c97c12b5ea97bbf85a
Devid Corsard <truegrinde@gmail.com>
2023-08-21 15:13:32 +0300

    hotkey for fixlist scrolling

--- lua/devidcorsard/remap.lua
+++ lua/devidcorsard/remap.lua
@@ -23,8 +23,8 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 --vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 --vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
---vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
---vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<Tab>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<S-Tab>", "<cmd>cprev<CR>zz")
 --vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 --vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
