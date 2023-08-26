b9a45b46dc02bde93f0c592875c95c64c1a1989c
devid-corsard <truegrinde@gmail.com>
2023-08-26 12:26:09 +0300

    tressiter beteer instal instructions

--- lua/devidcorsard/packer.lua
+++ lua/devidcorsard/packer.lua
@@ -17,7 +17,13 @@ return require('packer').startup(function(use)
 
     vim.cmd('colorscheme tokyonight')
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
     use('nvim-treesitter/playground')
     use('nvim-treesitter/nvim-treesitter-context')
 
